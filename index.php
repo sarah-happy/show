8b3734d5cef1a4750512d2e241a2f15e85709cc5
Sarah Happy <info@sarah-happy.ca>
2019-01-26 05:27:00 -0400

    refactor, adjust style

--- index.php
+++ index.php
@@ -1,67 +1,48 @@
-<!doctype html>
+<?php
+require './logic.php';
+?><!doctype html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="/show/node_modules/bootstrap/dist/css/bootstrap.css">
-<?php
-require './logic.php';
-
-$path_parts = split_request_uri();
-$path = '/' . implode('/', $path_parts);
-echo "<title>Index of ", htmlentities($path), "</title>\n";
-?>
-    <style type="text/css">
-        .highlight:hover {
-            background: lightblue;
-        }
-    </style>
+<meta name="viewport" 
+      content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<link rel="stylesheet" href="/show/style.css" />
+<title>Index of <?= html_request_uri() ?></title>
 </head>
 <body>
 <div class="container">
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="/">(root)</a></li>
-<?php
-$path = '';
-$path_url = '/';
-foreach($path_parts as $part) {
-    $path .= '/' . $part;
-    $path_url .= rawurlencode($part) . '/';
-    $part_html = htmlentities($part);
-    echo "<li class=\"breadcrumb-item\"><a href=\"$path_url\">$part_html</a></li>";
-}
-?>
+<?php foreach(split_request_uri() as $p) { ?>
+    <li class="breadcrumb-item"><a href="<?= $p['url'] ?>"><?= $p['html'] ?></a></li>
+<?php } ?>
     </ol>
     </nav>
     </div>
 
 <?php
-$files_path = real_request_dir();
 $files = list_request_dir();
-if($files) {
-    foreach($files as $file) {
-        echo '<div class="container highlight">';
-        $file_url = rawurlencode($file);
-        $file_html = htmlentities($file);
-        echo "<a href=\"$file_url\">$file_html</a>";
-        echo "<div class=\"text-right\">";
-
-        $file_path = $files_path . '/' . $file;
-        if(is_dir($file_path)) {
-            echo "(dir)";
-        }
-        if(is_file($file_path)) {
-            echo filesize($file_path);
-        }
-
-        echo "</div></div>\n";
+if($files === false) {
+?>
+    <div class="container">
+    Can not read folder.<br>
+    </div>
+<?php 
+}
+else {
+    foreach($files as $f) {
+?>
+    <div class="container">
+        <a class="fileitem" href="<?= $f['url'] ?>">
+            <div class="filename"><?= $f['html'] ?></div>
+            <div><?= $f['mtime'] ?> &mdash; <?= $f['size'] ?></div>
+        </a>
+    </div>
+<?php
     }
-} else {
-    echo "Can not read folder.<br>\n";
 }
 ?>
-
 <script src="/show/node_modules/jquery/dist/jquery.js"></script>
 <script src="/show/node_modules/popper.js/dist/umd/popper.js"></script>
 <script src="/show/node_modules/bootstrap/dist/js/bootstrap.js"></script>
--- logic.php
+++ logic.php
@@ -2,42 +2,94 @@
 // list the directory contents
 
 function split_request_uri() {
-    $request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $request_path = explode('/', $request_path);
     $out = array();
-    foreach($request_path as $part) {
-        if($part == '') {
+
+    $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    
+    $path = '';
+    $path_url = '/';
+    foreach(explode('/', $uri_path) as $name_url) {
+        if($name_url == '') {
             continue;
         }
-        $out[] = urldecode($part);
+        $path_url .= $name_url . '/';
+        $name = urldecode($name_url);
+        
+        $path .= '/' . $name;
+        $out[] = array(
+            'name' => $name,
+            'path' => $path,
+            'url' => $path_url,
+            'html' => htmlentities($name),
+        );
     }
+    
+    return $out;
+}
+
+function html_request_uri() {
+    $request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    return htmlentities(urldecode($request_path));
+}
+
+function real_request_dir() {
+    $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $path = urldecode($request_uri);
+    $real_path = $_SERVER['CONTEXT_DOCUMENT_ROOT'] . $path;
+    return $real_path;
+}
+
+function listdir($path) {
+    $out = array();
+    $dh = opendir($path);
+    if ( ! $dh ) {
+        return false;
+    }
+    while (false !== ($name = readdir($dh))) {
+        $out[] = $name;
+    }
+    closedir($dh);
     return $out;
 }
 
 function list_request_dir() {
-    $files = array();
-    if ($handle = opendir(real_request_dir())) {
-        while (false !== ($entry = readdir($handle))) {
-            if ($entry[0] == ".") {
-                continue;
-            }
-            if ($entry[0] == '$') {
-                continue;
-            }
-            $files[] = $entry;
-        }
-        closedir($handle);
-    } else {
+    $path = real_request_dir();
+    
+    $files = listdir($path);
+    if($files === false) {
         return false;
     }
+    
     sort($files);
-    return $files;
-}
 
-function real_request_dir() {
-    $path = '/' . implode('/', split_request_uri()) . '/';
-    $files_path = $_SERVER['CONTEXT_DOCUMENT_ROOT'] . $path;
-    return $files_path;
+    $out = array();
+    foreach($files as $name) {
+        if ($name[0] == "." or $name[0] == '$') {
+            continue;
+        }
+        
+        $file_path = $path . '/' . $name;
+        
+        $mtime = date("Y M d H:i:s", filemtime($file_path));
+        
+        if(is_dir($file_path)) {
+            $size = '(dir)';
+        } elseif(is_file($file_path)) {
+            $size = number_format(filesize($file_path)) . " bytes";
+        } else {
+            $size = "(???)";
+        }
+
+        $out[] = array(
+            'name' => $name,
+            'path' => $file_path,
+            'url' => rawurlencode($name),
+            'html' => htmlentities($name),
+            'mtime' => $mtime,
+            'size' => $size,
+        );
+    }
+    return $out;
 }
 
 ?>
